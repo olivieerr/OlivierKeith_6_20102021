09c8757e907b8b12975fd628ebb96991c20e4253
olivieerr <olivieerr@site-en-vrac.com>
2021-10-22 17:40:09 +0200

    Mise en place de la suppression de l'ancienne image en cas de modification de l'image

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -62,9 +62,16 @@ exports.modifySauce = (req, res, next) => {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : {...req.body};
-    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifiée' }))
-        .catch( error => res.status(400).json({ error }));
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
+                    .then(() => res.status(200).json({message: 'Sauce modifiée'}))
+                    .catch(error => res.status(400).json({error}));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
